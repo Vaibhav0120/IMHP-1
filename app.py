352a9a8dbe8cbbfdc9fc81130d0ec2fffa5f3323
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-03-18 17:18:12 +0000

    app fix

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from googletrans import Translator
 
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
